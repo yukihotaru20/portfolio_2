9c1209f2afe0dcde812e705c232fe83c19174051
[yukihotaru20] <[yukihotaru.co@gmail.com]>
2022-01-14 14:07:20 +0900

    ちょびっとソース整理

--- js/original.js
+++ js/original.js
@@ -1,43 +1,47 @@
-//ハンバーガーメニューのボタンとハンバーガーメニュー本体にアクティブを付与
+// ハンバーガーメニュー
 $(".openbtn1").click(function() {
     $(this).toggleClass('active');
     $(".header_menu").toggleClass('active');
 });
 
-//スクロールすると上部に固定させるための設定を関数でまとめる
+$(".header_menu_item a").click(function() {
+    $(".openbtn1").toggleClass('active');
+    $(".header_menu").toggleClass('active');
+});
+
+// ナビ固定＆背景色変更
 function FixedAnime() {
-    var headerH = $('.header').outerHeight(true); //.header_navのtopと同じ数値で減算させないとガクっとする
-    var header_navH = $('.header_nav').outerHeight(true) - 25; //.header_navのtopと同じ数値で減算させないとガクっとする
+    var headerH = $('.header').outerHeight(true);
+    var header_navH = $('.header_nav').outerHeight(true) - 25;
     var scroll = $(window).scrollTop();
-    if (scroll >= header_navH) { //header_navの高さ以上になったら
-        $('.header_nav').addClass('fixed'); //fixedというクラス名を付与
+    if (scroll >= header_navH) {
+        $('.header_nav').addClass('fixed');
     } else { //それ以外は
-        $('.header_nav').removeClass('fixed'); //fixedというクラス名を除去
+        $('.header_nav').removeClass('fixed');
     }
-    if (scroll >= headerH) { //header_navの高さ以上になったら
-        $('.header_nav').addClass('change'); //fixedというクラス名を付与
-        $('.openbtn1 span').addClass('change'); //fixedというクラス名を付与
+    if (scroll >= headerH) {
+        $('.header_nav').addClass('change');
+        $('.openbtn1 span').addClass('change');
     } else { //それ以外は
-        $('.header_nav').removeClass('change'); //fixedというクラス名を除去
-        $('.openbtn1 span').removeClass('change'); //fixedというクラス名を除去
+        $('.header_nav').removeClass('change');
+        $('.openbtn1 span').removeClass('change');
     }
 }
 
-//ナビゲーションをクリックした際のスムーススクロール
-$('.header_menu a').click(function() {
-    var elmHash = $(this).attr('href'); //hrefの内容を取得
-    var pos = Math.round($(elmHash).offset().top - 120); //headerの高さを引く
-    $('body,html').animate({ scrollTop: pos }, 500); //取得した位置にスクロール※数値が大きいほどゆっくりスクロール
-    return false; //リンクの無効化
-});
-
-
-// 画面をスクロールをしたら動かしたい場合の記述
+// スクロール時呼び出し
 $(window).scroll(function() {
-    FixedAnime(); /* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
+    FixedAnime();
 });
 
-// ページが読み込まれたらすぐに動かしたい場合の記述
+// ページ読み込み時呼び出し
 $(window).on('load', function() {
-    FixedAnime(); /* スクロール途中からヘッダーを出現させる関数を呼ぶ*/
+    FixedAnime();
+});
+
+// スムーススクロール
+$('.header_menu a').click(function() {
+    var elmHash = $(this).attr('href');
+    var pos = Math.round($(elmHash).offset().top - 120);
+    $('body,html').animate({ scrollTop: pos }, 500);
+    return false;
 });
